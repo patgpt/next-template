fdbcdf7cbce99354f781ecf200d378a53005a177
patgpt <doitformurphy@gmail.com>
2025-02-02 11:31:54 -0500

    feat: enhance routing configuration with navigation APIs for internationalization

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,15 +1,21 @@
 // i18n/routing.tsx
 
+import { createNavigation } from 'next-intl/navigation';
 import {defineRouting} from 'next-intl/routing';
 
+
+// Define the routing configuration
 export const routing = defineRouting({
     defaultLocale: 'en-US',
     locales: ['en-US', 'fr-CA'],
     localeDetection:true,
-
-  localeCookie: {
+    localeCookie: {
     // Expire in one year
     maxAge: 60 * 60 * 24 * 365
   }
 });
 
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration use these in place of the exports from 'next/router's
+export const {Link, redirect, usePathname, useRouter, getPathname} =
+  createNavigation(routing);
